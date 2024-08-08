0ac393042dc0e5b642b5f429fe7023d066b25fc8
sandh <sandh@SANDHYA>
2024-08-08 12:32:19 -0500

    AutoMapper configuration

--- src/Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ src/Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,7 +1,7 @@
-﻿using Mango.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
 using Mango.Services.CouponAPI.Models.Dto;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers
@@ -12,22 +12,23 @@ namespace Mango.Services.CouponAPI.Controllers
     {
         private readonly AppDbContext _appDbContext;
         private ResponseDto _response;
+        private IMapper _mapper;
 
-        public CouponAPIController(AppDbContext appDbContext)
+        public CouponAPIController(AppDbContext appDbContext, IMapper mapper)
         {
             _appDbContext = appDbContext;
             _response = new ResponseDto();  
+            _mapper = mapper;
         }
 
         [HttpGet]
-
         public ResponseDto Get()
         {
             try
             {
                 IEnumerable<Coupon> couponList = _appDbContext.Coupons.ToList();
 
-                _response.Result = couponList;  
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(couponList);  
             }
             catch (Exception ex) 
             { 
@@ -46,7 +47,7 @@ namespace Mango.Services.CouponAPI.Controllers
             try
             {
                 Coupon coupon = _appDbContext.Coupons.First(x => x.CouponId == id);
-                _response.Result = coupon;
+                _response.Result = _mapper.Map<CouponDto>(coupon);
             }
             catch (Exception ex)
             {
--- src/Mango/Mango.Services.CouponAPI/Program.cs
+++ src/Mango/Mango.Services.CouponAPI/Program.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using Mango.Services.CouponAPI;
 using Mango.Services.CouponAPI.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +12,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 });
+
+//// AutoMapper configuration
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+// register the mapper to the services
+builder.Services.AddSingleton(mapper);
+// use automapper using dependency injection
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
