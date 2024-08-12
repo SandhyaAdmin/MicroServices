138de45406393baaa1db9ec85856566d8ea2cc76
sandh <sandh@SANDHYA>
2024-08-12 12:17:41 -0500

    Implemented BaseService and IBaseService

---
+++ src/Mango/Mango.Web/Service/BaseService.cs
@@ -0,0 +1,79 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Text;
+using static Mango.Web.Utility.StaticDetails;
+using System.Net;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory; 
+        }
+       public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+       { 
+            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+
+            HttpRequestMessage message = new();
+            message.Headers.Add("Content-Type", "application/json");
+            message.RequestUri = new Uri(requestDto.Url);
+
+            if (requestDto != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data),Encoding.UTF8, "application/json");
+            }
+            //token
+            HttpResponseMessage? apiResponse = null;
+            switch (requestDto.ApiType)
+            {
+                case ApiType.POST:
+                    message.Method = HttpMethod.Post;
+                    break;
+                case ApiType.PUT:
+                    message.Method = HttpMethod.Put;
+                    break;
+                case ApiType.DELETE:
+                    message.Method = HttpMethod.Delete;
+                    break;
+                default: 
+                    message.Method = HttpMethod.Get;
+                    break;
+            }
+;
+            apiResponse = await client.SendAsync(message);
+            try
+            {
+                switch (apiResponse.StatusCode)
+                {
+                    case HttpStatusCode.NotFound:
+                        return new() { IsSuccess = false, Message = "Not Found" };
+                    case HttpStatusCode.Forbidden:
+                        return new() { IsSuccess = false, Message = "Access Denied" };
+                    case HttpStatusCode.Unauthorized:
+                        return new() { IsSuccess = false, Message = "Unauthorized" };
+                    case HttpStatusCode.InternalServerError:
+                        return new() { IsSuccess = false, Message = "Internal Server Error" };
+                    default:
+                        var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                        var apiResponseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+                        return apiResponseDto;
+
+                }
+            }
+            catch (Exception ex) 
+            { 
+                var dto = new ResponseDto
+                {
+                    Message = ex.Message.ToString(),
+                    IsSuccess = false
+
+                };
+                return dto; 
+            }
+       }
+    }
+}
--- src/Mango/Mango.Web/Utility/StaticDetails.cs
+++ src/Mango/Mango.Web/Utility/StaticDetails.cs
@@ -8,7 +8,6 @@
             POST,
     	    PUT,
     	    DELETE
-
         }
 
     }
