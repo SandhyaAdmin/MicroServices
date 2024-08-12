2217df921f2b4d93ad45626acf56d371c04abd5c
sandh <sandh@SANDHYA>
2024-08-12 11:03:39 -0500

    Creating Mango.Web Project with Request and Response Dto

---
+++ src/Mango/Mango.Web/Utility/StaticDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Mango.Web.Utility
+{
+    public class StaticDetails
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+    	    PUT,
+    	    DELETE
+
+        }
+
+    }
+}
