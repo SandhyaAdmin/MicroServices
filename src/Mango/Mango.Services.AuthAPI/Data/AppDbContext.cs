998f79028b0e7ab28fa3a9e8d2395b9db141e0d0
sandh <sandh@SANDHYA>
2024-08-27 16:18:23 -0500

    Configuring Identity

---
+++ src/Mango/Mango.Services.AuthAPI/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.AuthAPI.Data
+{
+
+    /* Typically when you are working with Entity Framework core, we only needed DBContext,
+     but here, we will be using dot net identity, To use Identity for authentication and authorization
+    we need to implement AppDbContext from IdentityDbContext
+    */
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+        // this method is used to seed the record in a database table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
--- src/Mango/Mango.Services.AuthAPI/Program.cs
+++ src/Mango/Mango.Services.AuthAPI/Program.cs
@@ -1,6 +1,28 @@
+using Mango.Services.AuthAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Confifuguring the DbContext Service for Sql Connection with EF Core
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString(""));
+});
+
+/*
+ * Note : Here, we need to tell the EF Core that we will be using .Net Identity and .EF Core
+ * where we have the DBContext with .Net Identity
+ * That is how it will know to use DBContext to create all the Identity related tables
+ */
+
+// Using Default user and default role , AddEntityFrameworkStores -> acts as a bridge b/w
+// dot net Identity core and Entity Framework core
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,7 +30,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+    
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -18,8 +40,32 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// As AuthAPI is responsible for Authentication and Authorization, include UseAuthenticatuin()
+// Note : In the pipeline, Authentication() must always come before Authorization()
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+
+// This method will run all the pending migrations, when the app gets started instead of using update-database
+void ApplyMigration()
+{
+
+    using (var scope = app.Services.CreateScope())  // this line of code gets all the services
+    {
+        // Get application DBContext here and check if there are any pending migrations
+
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
