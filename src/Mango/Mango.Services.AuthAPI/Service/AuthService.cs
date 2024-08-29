a76c1c65d912c581ab8b0d51824174b38a333270
sandh <sandh@SANDHYA>
2024-08-28 19:57:56 -0500

    Implemented IAuthService

---
+++ src/Mango/Mango.Services.AuthAPI/Service/AuthService.cs
@@ -0,0 +1,34 @@
+﻿using Mango.Services.AuthAPI.Data;
+using Mango.Services.AuthAPI.Models;
+using Mango.Services.AuthAPI.Models.Dto;
+using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Services.AuthAPI.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _db;
+
+        // using Helper classes/mehods available in Identity to regiter and save data to ASPNetRoles
+        private readonly  UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManger;
+
+        private AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManger)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManger = roleManger;
+        }
+
+        public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
