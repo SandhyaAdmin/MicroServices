f116b52ae1a95bf1f2cd49c6dbeeb6d376b11f4d
sandh <sandh@SANDHYA>
2024-09-11 13:33:44 -0500

    Sign in user in Dot net Identity

--- src/Mango/Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
+++ src/Mango/Mango.Services.AuthAPI/Controllers/AuthAPIController.cs
@@ -43,7 +43,8 @@ namespace Mango.Services.AuthAPI.Controllers
                 _response.Message = "UserName and Password is InCorrect";
                 return BadRequest(_response);   
             }
-            return Ok(loginResponse);
+            _response.Result = loginResponse;
+            return Ok(_response);
         }
 
         [HttpPost("AssignRole")]
--- src/Mango/Mango.Web/Controllers/AuthController.cs
+++ src/Mango/Mango.Web/Controllers/AuthController.cs
@@ -1,19 +1,25 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
 using Mango.Web.Utility;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Mango.Web.Controllers
 {
     public class AuthController : Controller
     {
         private readonly IAuthService _authService;
+        private readonly ITokenProvider _tokenProvider;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService, ITokenProvider tokenProvider)
         {
             _authService = authService;
+            _tokenProvider = tokenProvider;
         }
 
         [HttpGet]
@@ -74,6 +80,11 @@ namespace Mango.Web.Controllers
             if (responseDto != null && responseDto.IsSuccess)
             {
                 LoginResponseDto? loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+
+                await SignInUser(loginResponseDto);
+
+                _tokenProvider.SetToken(loginResponseDto.Token);
+
                 return RedirectToAction("Index", "Home");
             }
             else
@@ -88,6 +99,33 @@ namespace Mango.Web.Controllers
         {
             return View();
         }
+
+        private async Task SignInUser(LoginResponseDto loginResponseDto)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            var jwt = handler.ReadJwtToken(loginResponseDto.Token);
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Email,
+                jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Email).Value));
+
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub,
+                 jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Sub).Value));
+
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Name,
+                 jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Name).Value));
+
+
+            identity.AddClaim(new Claim(ClaimTypes.Name,
+                jwt.Claims.FirstOrDefault(x => x.Type == JwtRegisteredClaimNames.Email).Value));
+
+
+
+            var principle = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principle);
+        }   
 
     }
 
