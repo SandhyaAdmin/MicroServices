db1cf7b9b91b01861bd75f09ddacbea05f4e2ca0
sandh <sandh@SANDHYA>
2024-09-11 12:44:01 -0500

    token Provider Service and Storage of token in cookies implemetation

---
+++ src/Mango/Mango.Web/Service/TokenProvider.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Newtonsoft.Json.Linq;
+
+namespace Mango.Web.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        /* using cookies to store the tokens as we send tokens while requesting the apis, when workig with cookies need to use
+         * IHttpContextAccessor, already configured that in program.cs
+         */
+
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TokenProvider(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+        public void SetToken(string token)
+        {
+            _contextAccessor.HttpContext?.Response.Cookies.Append(StaticDetails.TokenCookie, token);
+        }
+
+        public string? GetToken()
+        {
+            string? token = null;
+            bool? hasToken = _contextAccessor.HttpContext?.Request.Cookies.TryGetValue(StaticDetails.TokenCookie, out token);
+
+            return hasToken is true ? token : null;
+        }
+
+        public void ClearToken()
+        {
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(StaticDetails.TokenCookie);
+        }
+
+    }
+}
