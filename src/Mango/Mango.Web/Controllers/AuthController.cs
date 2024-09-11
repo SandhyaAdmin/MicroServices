80f90ffcecf4fd797d3d96a6c834f5cbaf6347c2
sandh <sandh@SANDHYA>
2024-09-11 12:17:20 -0500

    Register Controller with role assignment

--- src/Mango/Mango.Web/Controllers/AuthController.cs
+++ src/Mango/Mango.Web/Controllers/AuthController.cs
@@ -28,6 +28,36 @@ namespace Mango.Web.Controllers
             return View();
         }
 
+        [HttpPost]  
+        public async Task<IActionResult> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            ResponseDto? result = await _authService.RegisterAsync(registrationRequestDto);
+            ResponseDto? assignRole = null;
+            if (result != null && result.IsSuccess)
+            {
+                if (string.IsNullOrEmpty(registrationRequestDto.Role))
+                {
+                    registrationRequestDto.Role = StaticDetails.RoleCustomer;  
+                }
+                assignRole = await _authService.AssignRoleAsync(registrationRequestDto);
+                if (assignRole != null && assignRole.IsSuccess) 
+                {
+                    TempData["Success"] = "Registration Successfull";
+                    return RedirectToAction(nameof(Login));
+                }
+
+            }
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text = StaticDetails.RoleAdmin, Value = StaticDetails.RoleAdmin},
+                new SelectListItem{Text = StaticDetails.RoleCustomer, Value = StaticDetails.RoleCustomer},
+            };
+
+            ViewBag.RoleList = roleList;
+            return View(registrationRequestDto);
+
+        }
+
         [HttpGet]
         public IActionResult Login()
         {
