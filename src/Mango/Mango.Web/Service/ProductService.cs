d2dd2a035c8578845650f782a4f86129c310d10f
sandh <sandh@SANDHYA>
2024-09-17 00:02:18 -0500

    Product Service implementation

---
+++ src/Mango/Mango.Web/Service/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class ProductService : IProductService
+    {
+        public Task<ResponseDto?> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetProductByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ResponseDto?> CreateProductAsync(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task<ResponseDto?> UpdateProductAsync(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ResponseDto?> DeleteProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
