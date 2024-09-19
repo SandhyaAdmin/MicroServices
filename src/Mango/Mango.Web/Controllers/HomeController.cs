0c23edcab600808d2ceec271f42ee85cb98497e3
sandh <sandh@SANDHYA>
2024-09-18 23:11:04 -0500

    Home Page and details

--- src/Mango/Mango.Web/Controllers/HomeController.cs
+++ src/Mango/Mango.Web/Controllers/HomeController.cs
@@ -1,23 +1,60 @@
 using Mango.Web.Models;
+using Mango.Web.Service;
+using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IProductService _productService;
+        private ResponseDto _responseDto;
+           
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
+            _responseDto = new ResponseDto();
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDto> productList = new List<ProductDto>();
+
+             _responseDto = await _productService.GetAllProductsAsync();
+
+            if (_responseDto != null && _responseDto.IsSuccess)
+            {
+                productList = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(_responseDto.Result));
+            }
+            else
+            {
+                TempData["error"] = _responseDto?.Message;
+            }
+
+            return View(productList);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            ProductDto productDto = new ProductDto();
+
+            _responseDto = await _productService.GetProductByIdAsync(productId);
+
+            if (_responseDto != null && _responseDto.IsSuccess)
+            {
+                productDto = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(_responseDto.Result));
+            }
+            else
+            {
+                TempData["error"] = _responseDto?.Message;
+            }
+
+            return View(productDto);
         }
         public IActionResult Privacy()
         {
