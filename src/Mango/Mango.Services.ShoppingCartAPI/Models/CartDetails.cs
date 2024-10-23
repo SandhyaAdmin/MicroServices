692408ff752eb4f978bea1c4a89bef600f845502
sandh <sandh@SANDHYA>
2024-10-23 17:09:30 -0500

    Added AppDbContext, Extenstions and MappingConfig

--- src/Mango/Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
+++ src/Mango/Mango.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Mango.Services.ShoppingCartAPI.Models.Dto
+using Mango.Services.ShoppingCartAPI.Models.Dto;
 
 namespace Mango.Services.ShoppingCartAPI.Models
 {
--- src/Mango/Mango.Services.ShoppingCartAPI/Program.cs
+++ src/Mango/Mango.Services.ShoppingCartAPI/Program.cs
@@ -1,11 +1,61 @@
+using AutoMapper;
+using Mango.Services.ShoppingCartAPI;
+using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Confifuguring the DbContext Service for Sql Connection with EF Core
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
+
+//// AutoMapper configuration
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+// register the mapper to the services
+builder.Services.AddSingleton(mapper);
+// use automapper using dependency injection
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+// Enabling authentication in swagger generation
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following : `Bearer Generated-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {{
+                new OpenApiSecurityScheme
+        {
+            Reference=  new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = JwtBearerDefaults.AuthenticationScheme
+            }
+        }, new string[]{}
+        }
+    });
+});
+
+builder.AddAppAuthentication();
+builder.Services.AddAuthentication();
 
 var app = builder.Build();
 
@@ -18,8 +68,31 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// If we are authenticating, we are validating 3 things, We have the secret, we have the issuer and we have the audience, we have to validate our token using all 3 of them
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+
+// This method will run all the pending migrations, when the app gets started instead of using update-database
+void ApplyMigration()
+{
+
+    using (var scope = app.Services.CreateScope())  // this line of code gets all the services
+    {
+        // Get application DBContext here and check if there are any pending migrations
+
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
