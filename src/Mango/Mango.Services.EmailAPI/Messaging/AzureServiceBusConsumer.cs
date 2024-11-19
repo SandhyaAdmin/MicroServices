e7fbb5f55427065ff3c7680c6a644a96360a39b8
sandh <sandh@SANDHYA>
2024-11-18 23:48:24 -0600

    Consuming Azure Service Bus Queue

---
+++ src/Mango/Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -0,0 +1,20 @@
+﻿namespace Mango.Services.EmailAPI.Messaging
+{
+    public class AzureServiceBusConsumer
+    {
+        private readonly string? serviceBusConnectionString;
+
+        private readonly string? emaiCartQueueName;
+
+        private readonly IConfiguration _configuration;
+
+        public AzureServiceBusConsumer(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
+
+            emaiCartQueueName = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+        }
+    }
+}
