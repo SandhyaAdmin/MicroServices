7492f9c06a93118f15a81409f43304ff96a7ad40
sandh <sandh@SANDHYA>
2024-11-18 23:29:33 -0600

    Added required model Dtos

---
+++ src/Mango/Mango.Services.EmailAPI/Models/Dto/CartDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http.Headers;
+
+namespace Mango.Services.EmailAPI.Models.Dto
+{
+    public class CartDetailsDto
+    {
+        public int CartDetaildId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeaderDto? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+
+        public int Count { get; set; }
+    }
+}
