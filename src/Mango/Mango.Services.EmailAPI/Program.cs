b85d1af977b45640781c6f746b40f30976fa6af9
sandh <sandh@SANDHYA>
2024-11-18 23:11:28 -0600

    DBContextClass, EmailLoggerDBSet,appsettings.json,registering the servive,add migration

--- src/Mango/Mango.Services.EmailAPI/Program.cs
+++ src/Mango/Mango.Services.EmailAPI/Program.cs
@@ -1,3 +1,6 @@
+using Mango.Services.EmailAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
+});
 
 var app = builder.Build();
 
@@ -22,4 +30,24 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+ApplyMigration();
+
 app.Run();
+
+
+// This method will run all the pending migrations, when the app gets started instead of using update-database
+void ApplyMigration()
+{
+
+    using (var scope = app.Services.CreateScope())  // this line of code gets all the services
+    {
+        // Get application DBContext here and check if there are any pending migrations
+
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
