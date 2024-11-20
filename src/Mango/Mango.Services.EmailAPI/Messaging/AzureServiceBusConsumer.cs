bfe1f7643ea1d8923436b6458baa12a513da3341
sandh <sandh@SANDHYA>
2024-11-19 22:03:01 -0600

    Craeted start and stop methods for teh event handling

--- src/Mango/Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ src/Mango/Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,11 @@
 ﻿using Azure.Messaging.ServiceBus;
+using Mango.Services.EmailAPI.Models.Dto;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Mango.Services.EmailAPI.Messaging
 {
-    public class AzureServiceBusConsumer
+    public class AzureServiceBusConsumer : IAzureServiceBusConsumer
     {
         private readonly string? serviceBusConnectionString;
 
@@ -23,6 +26,48 @@ namespace Mango.Services.EmailAPI.Messaging
             var client = new ServiceBusClient(serviceBusConnectionString);
             _emailCartProcessor = client.CreateProcessor(emaiCartQueueName);
 
+            // we need to register the handler for the processor we have created
+
         }
+
+        public async Task Start()
+        {
+            _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+            _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+        }
+
+
+        public async Task Stop()
+        {
+           await _emailCartProcessor.StartProcessingAsync();
+           await _emailCartProcessor.DisposeAsync();
+        }
+
+        public async Task OnEmailCartRequestReceived(ProcessMessageEventArgs args)
+        {
+            // This is where you will receive the message
+            var message = args.Message;
+            var body = Encoding.UTF8.GetString(message.Body);   
+            CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(body);
+            try
+            {
+                //TODO - try to log email
+                //the below line tells the queue that message has been processed successfully and you can remove that from your queue
+                await args.CompleteMessageAsync(args.Message);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+
+        
     }
 }
