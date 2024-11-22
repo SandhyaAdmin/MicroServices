5126b03e1613d87da6e04047e47992bd3745d8e2
sandh <sandh@SANDHYA>
2024-11-22 17:49:01 -0600

    Added required Order Models and Dtos

---
+++ src/Mango/Mango.Services.OrderAPI/Models/Dto/OrderDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Mango.Services.OrderAPI.Models.Dto
+{
+    public class OrderDetailsDto
+    {
+        public int OrderDetaildId { get; set; }
+        public int OrderHeaderId { get; set; }
+
+       // Commenting out the below one as it has IEnumerable, there might be chances of entering into infinite loop
+        // public OrderHeader? OrderHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDto? Product { get; set; }
+
+        public int Count { get; set; }
+
+        // If the price of the product got updated when the product placed, we want to include that
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ src/Mango/Mango.Services.OrderAPI/Models/OrderDetails.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Mango.Services.OrderAPI.Models.Dto;
+
+namespace Mango.Services.OrderAPI.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public int OrderDetaildId { get; set; }
+
+        public int OrderHeaderId { get; set; }
+
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader? OrderHeader { get; set; }
+        public int ProductId { get; set; }
+
+        [NotMapped]
+        public ProductDto? Product { get; set; }
+
+        public int Count { get; set; }
+
+        // If the price of the product got updated when the product placed, we want to include that
+        public string ProductName { get; set; } 
+        public double Price { get; set; }
+    }
+}
