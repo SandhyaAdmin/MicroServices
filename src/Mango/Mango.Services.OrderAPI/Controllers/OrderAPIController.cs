a005ede7863b428433a6d52cade33b5feca85cd5
sandh <sandh@SANDHYA>
2024-12-30 17:36:39 -0600

    Created OrderCreate End point

---
+++ src/Mango/Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Service.Iservice;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mango.Services.OrderAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Mango.Services.OrderAPI.Utility;
+using Mango.Services.OrderAPI.Models;
+
+namespace Mango.Services.OrderAPI.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+        private IProductService _productService;
+
+        public OrderAPIController(ResponseDto response, IMapper mapper, AppDbContext db, IProductService productService)
+        {
+            _response = response;
+            _mapper = mapper;
+            _db = db;
+            _productService = productService;
+        }
+
+        [Authorize]
+        [HttpPost("CreateDto")]
+        public async Task<ResponseDto> CreateDto([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.Now;
+                orderHeaderDto.Status = StaticDetails.Status_Pending;
+                // Initially, status is pending, once the payment is processed, will change the status to approved
+
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+                
+                // If want to access the Entity, I cam use entity
+                OrderHeader orderCreated = await _db.OrderHeaders.AddAsync(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+
+                await _db.SaveChangesAsync();
+
+               orderHeaderDto.OrderHeaderId = orderCreated.OrderHeaderId;
+               _response.Result = orderHeaderDto;
+
+            }
+            catch (Exception ex) 
+            { 
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;             
+            }
+
+            return _response;
+        }
+    }
+}
