e4ee32511a85341d9598cda7066c0c4fc52b0e26
sandh <sandh@SANDHYA>
2024-12-30 16:57:40 -0600

    Mapping Config for order api

--- src/Mango/Mango.Services.OrderAPI/MappingConfig.cs
+++ src/Mango/Mango.Services.OrderAPI/MappingConfig.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
 
 
 namespace Mango.Services.OrderAPI
@@ -8,6 +10,21 @@ namespace Mango.Services.OrderAPI
         public static MapperConfiguration RegisterMaps()
         {
             var mappingConfig = new MapperConfiguration(config => {
+                // As the order to created of type shopping cart, CartTotal must go to Order total
+                config.CreateMap<OrderHeaderDto, CartHeaderDto>()
+                .ForMember(dest => dest.CartTotal, u=> u.MapFrom(src => src.OrderTotal)).ReverseMap();
+
+                // Retrive Product name and price from CartDeatils Dto and assigned to OrderDetailsDto
+                config.CreateMap<CartDetailsDto, OrderDetailsDto>()
+                .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+
+                // In reverse mapping we dont care about the above scanarion, So, we can do directly
+                config.CreateMap<OrderDetailsDto, CartDetailsDto>();
+
+                config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+
+                config.CreateMap<OrderDetailsDto,OrderDetailsDto>().ReverseMap();
             });
 
             return mappingConfig;
