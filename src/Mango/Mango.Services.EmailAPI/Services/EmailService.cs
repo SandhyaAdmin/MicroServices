f9184e7ca34fe998246834aaabd3f2451752dd87
sandh <sandh@SANDHYA>
2025-05-14 22:39:42 -0500

    LogOrderPlaced method in Email Service

--- src/Mango/Mango.Services.EmailAPI/Services/EmailService.cs
+++ src/Mango/Mango.Services.EmailAPI/Services/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using Mango.Services.EmailAPI.Data;
+using Mango.Services.EmailAPI.Message;
 using Mango.Services.EmailAPI.Models;
 using Mango.Services.EmailAPI.Models.Dto;
 using Microsoft.EntityFrameworkCore;
@@ -30,6 +31,13 @@ namespace Mango.Services.EmailAPI.Services
             message.Append("</ul>");
 
             await LogAndEmail(message.ToString(), cartDto.CartHeader.Email);
+        }
+
+        public async Task LogOrderPlaced(RewardMessage rewardMessage)
+        {
+            string message = "New order placed. </br> OrderID" + rewardMessage.OrderId;
+
+            await LogAndEmail(message, "dotnetmastery@gmail.com");
         }
 
         public async Task RegisterUserEmailAndLog(string email)
