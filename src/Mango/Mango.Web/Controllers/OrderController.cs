6ef01c384ede25c9373959a76b15d5477f38567f
sandh <sandh@SANDHYA>
2025-05-22 22:48:09 -0500

    Confugure Database end point

--- src/Mango/Mango.Web/Controllers/OrderController.cs
+++ src/Mango/Mango.Web/Controllers/OrderController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Models.Dto;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using Mango.Web.Models;
+using Newtonsoft.Json;
 
 namespace Mango.Web.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;   
+        }
         public IActionResult OrderIndex()
         {
             return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll() 
+        {
+            IEnumerable<OrderHeaderDto>? list;
+
+            string userId = string.Empty;
+
+            if (!User.IsInRole(StaticDetails.RoleAdmin))
+            {
+                userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault().ToString();
+
+            }
+
+            ResponseDto?  responseDto = _orderService.GetAllOrders(userId).GetAwaiter().GetResult();
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<OrderHeaderDto>>(Convert.ToString(responseDto?.Result));
+            }
+            else
+            {
+                list = new List<OrderHeaderDto>();
+            }
+
+            return Json(new { data = list });
+
         }
     }
 }
---
+++ src/Mango/Mango.Web/wwwroot/js/Order.js
@@ -0,0 +1,24 @@
+﻿var dataTable;
+function loadDataTable() {
+/*    using the query selector, we need to get the id, using order index, where I have added tblData, we access that here, on that we will call the .DataTable() that is avaible because we configured datatable.net 
+JS file in Layou.cshtmlt for datatables dotnet*, there we have few basic configurations, when we make the ajax calls
+ what url should it go to, add that end point in order controller to retrive all the records.
+ 
+ Final Conclusuion, when this method gets called in cshtml, using ajax calls, it retrived the data from end point
+ and assingns the values to the table with id tblData.
+
+ Note : As we are using dot net core with MVC, we have built in api supprort, where we access the end point
+ */
+
+    dataTable = $('#tblData').dataTable({
+        "ajax": {
+            url: "/order/getall"
+        },
+        //    next we need to defined the columns in our datable which is an array
+        "columns": [
+            { data: 'orderheaderid', 'width': "5%"},
+            { data: 'email', 'width': "25%"},
+        ]
+
+    })
+}
