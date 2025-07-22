7dba1808bb57e5178935245252c0fb3eb2e9bed7
sandh <sandh@SANDHYA>
2025-07-21 22:51:31 -0500

    Upload Image on create Product

--- src/Mango/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ src/Mango/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -61,7 +61,7 @@ namespace Mango.Services.ProductAPI.Controllers
 
         [HttpPost]
         [Authorize(Roles = "ADMIN")]
-        public ResponseDto Post([FromBody] ProductDto productDto)
+        public ResponseDto Post(ProductDto productDto)
         {
             try
             {
