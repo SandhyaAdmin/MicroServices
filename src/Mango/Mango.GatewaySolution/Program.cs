b036393bdbe67d9bb1d71bc068041f04e4b8307b
sandh <sandh@SANDHYA>
2025-07-29 22:20:10 -0500

    Configured applocation to use ocelot

--- src/Mango/Mango.GatewaySolution/Program.cs
+++ src/Mango/Mango.GatewaySolution/Program.cs
@@ -1,6 +1,12 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// registering ocelot in the application, adds ocelots to the conatiner
+builder.Services.AddOcelot();
 
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot();
 app.Run();
