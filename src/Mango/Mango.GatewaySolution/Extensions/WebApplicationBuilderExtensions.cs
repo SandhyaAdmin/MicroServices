579c8f0022c8092a6f83866bdf84a3dcf55f57ff
sandh <sandh@SANDHYA>
2025-07-29 22:34:58 -0500

    Add authentication to ocelot

---
+++ src/Mango/Mango.GatewaySolution/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.Middleware;
+using System.Text;
+
+namespace Mango.Services.GatewaySolution.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder)
+        {
+            // we need to add authentication
+            var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+            var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
+            var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
+
+            var key = Encoding.ASCII.GetBytes(secret);
+
+           // Adds JWT Bearer authentication to the DI container and sets it as the default scheme.
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                /* 
+                 * Configures the token validation parameters:
+                 * <br>– Checks signature with IssuerSigningKey
+                 * <br>– Verifies token Issuer and Audience 
+                 * <br>– Enables basic validation to reject invalid or tampered tokens
+                 */
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    ValidateAudience = true
+                };
+            });
+
+            return builder;
+        }
+    }
+}
--- src/Mango/Mango.GatewaySolution/Program.cs
+++ src/Mango/Mango.GatewaySolution/Program.cs
@@ -1,12 +1,21 @@
+using Mango.Services.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
 // registering ocelot in the application, adds ocelots to the conatiner
 builder.Services.AddOcelot();
+// it’s the perfect hook to ensure downstream APIs only process valid and secure JWTs
+builder.AddAppAuthentication();
+var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.UseOcelot();
 app.Run();
+
+
+/* add authentication to ocelot, the reason to add authentication to ocelot is
+ * from the web project when we are calling the gateway, it needs to pass the jwt tokens to
+ * the individual web services, only then api return back the repsonse.
+ * */
+
