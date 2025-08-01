aefe682ae3ac19eb9689b69ec4d3f354e8226f57
sandh <sandh@SANDHYA>
2025-07-31 22:40:13 -0500

    Add first Ocelot route

--- src/Mango/Mango.GatewaySolution/Program.cs
+++ src/Mango/Mango.GatewaySolution/Program.cs
@@ -5,6 +5,11 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 // registering ocelot in the application, adds ocelots to the conatiner
 builder.Services.AddOcelot();
+
+/* add authentication to ocelot, the reason to add authentication to ocelot is
+ * from the web project when we are calling the gateway, it needs to pass the jwt tokens to
+ * the individual web services, only then api return back the repsonse.
+ * */
 // it’s the perfect hook to ensure downstream APIs only process valid and secure JWTs
 builder.AddAppAuthentication();
 var app = builder.Build();
@@ -14,8 +19,8 @@ app.UseOcelot();
 app.Run();
 
 
-/* add authentication to ocelot, the reason to add authentication to ocelot is
- * from the web project when we are calling the gateway, it needs to pass the jwt tokens to
- * the individual web services, only then api return back the repsonse.
- * */
+/* When the rquest comes from the web project with the api url, we need to tell the ocelot
+ * which api it should call exatly.
+ */
+
 
